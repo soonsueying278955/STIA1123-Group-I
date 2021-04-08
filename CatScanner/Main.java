6e688292990e4ea142589894ba8553a64210da40
sueying <72725592+soonsueying278955@users.noreply.github.com>
2021-04-09 00:01:53 +0800

    Add files via upload

---
+++ CatScanner/Main.java
@@ -0,0 +1,14 @@
+package CatScanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cat PersianCat = new Cat();
+		
+		PersianCat.profile();
+		PersianCat.calculateFoodCost();
+		PersianCat.calculateClothesCost();
+		PersianCat.calculateSpeed();
+	}
+
+}
