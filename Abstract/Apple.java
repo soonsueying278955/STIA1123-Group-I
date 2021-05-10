0d0420195addb519cb58ca5a18b811b66fad9a3a
sueying <72725592+soonsueying278955@users.noreply.github.com>
2021-05-10 10:55:27 +0800

    Add files via upload

--- Abstract/Apple.java
+++ Abstract/Apple.java
@@ -18,13 +18,17 @@ public class Apple extends Fruit implements Discount{
 		}
 		
 		else if(this.quantity > 10 && this.quantity < 50) {
+			double pp = 1.59; //purchase more than 10 but less than 50, the price is RM 1.59 
+			totalPrice(pp); //overloading with 1 argument
 			System.out.println("If quantity is more than 10 but less than 50, you will not get the discount !!");
-			System.out.printf("Total price is " + "RM " + "%.2f", totalPrice());
+			System.out.printf("Total price is " + "RM " + "%.2f", totalPrice(pp));
 		}
 		
 		else {
+			double pp = 0.59; //purchase more than 50, the price is RM0.59
+			double d = 0.1; //discount 10% for purchase more than 50
 			System.out.println("If quantity is more than 50, you will get a 10% discount !!");
-			System.out.printf("Total price is " + "RM " + "%.2f", totalPrice());
+			System.out.printf("Total price is " + "RM " + "%.2f", totalPrice(pp,d));
 			}
 		}
 	public double rate_of_discount() {
@@ -32,7 +36,7 @@ public class Apple extends Fruit implements Discount{
 			discount = 0;
 		}
 		else if(quantity > 10 && quantity < 50) {
-				discount = 0;
+			discount = 0;
 		}
 		else {
 			discount = 0.1;
