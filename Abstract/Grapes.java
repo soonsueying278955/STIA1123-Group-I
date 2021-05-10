099b2570351675eecf314d1067d6aa3e59037a09
sueying <72725592+soonsueying278955@users.noreply.github.com>
2021-05-10 09:53:11 +0800

    Add files via upload

---
+++ Abstract/Grapes.java
@@ -0,0 +1,71 @@
+package Abstract;
+
+public class Grapes extends Fruit{
+	private int water, kilogram;
+	private double discount;
+	
+	public Grapes(String N, int W, int K) {
+		super(N);
+		this.water = W;
+		this.kilogram = K;
+		
+		
+		switch(kilogram) {
+		case 10 :
+			System.out.println("Total water in " + kilogram + "g of strawberry " + "is " + totalWater() + "%");
+			break;
+			
+		case 20:
+			System.out.println("Total water in " + kilogram + "kg of grapes " + "is " + totalWater() + "%");
+			break;
+		}
+	}
+	
+	public double rate_of_discount() {
+		if(kilogram <= 10) {
+			System.out.println("If kilogram is less than 10, you will not get the discount !!");
+			discount = 0;
+		}
+		else if (kilogram >= 20) {
+			System.out.println("If quantity is more than 10, you will get 5% discount !!");
+			discount = 0.15;
+		}
+		return discount;
+	}
+
+	public int totalWater() {
+		return water;
+	}
+	
+	public int totalWater(int K) {
+		return K;
+	}
+	
+	
+	//overloading with no arguments
+	public double totalPrice() {
+		return 0;
+	}
+	
+	//overloading with no arguments
+	public double totalPrice(double P) {
+		return P * this.kilogram;
+	}
+		
+	//overloading with no arguments
+	public double totalPrice(double P, double d) {
+		return (P * this.kilogram) - ((P * this.kilogram) - (d/100));
+	} 
+		
+	//overriding method
+	public String printBenefitEating() { 
+		return "Eating grapes may decrease blood sugar levels and also protect against diabetes.";
+	}
+	
+	//overriding method
+	public String toString() {
+		return printBenefitEating() + "\nDiscount rate\t = " + rate_of_discount();
+	}
+
+	
+}
